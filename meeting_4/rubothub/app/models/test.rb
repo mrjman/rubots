0dc510ed497797145c42f9264080d4a7328e6ec3
Jesse <jesse@mondorobot.com>
2015-11-13 13:45:08 -0700

    remove tests model files

--- meeting_4/rubothub/app/models/test.rb
+++
@@ -1,3 +0,0 @@
-class Test < ActiveRecord::Base
-  validates :title, presence: true
-end
--- meeting_4/rubothub/db/migrate/20151112170142_create_tests.rb
+++
@@ -1,9 +0,0 @@
-class CreateTests < ActiveRecord::Migration
-  def change
-    create_table :tests do |t|
-      t.string :title
-
-      t.timestamps null: false
-    end
-  end
-end
