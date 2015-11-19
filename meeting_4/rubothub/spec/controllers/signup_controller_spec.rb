1b2b6ba4bae266544f7143bce47e918fc838af1e
cprater <cprater23@gmail.com>
2015-11-19 13:22:35 -0700

    Controller spec

--- meeting_4/rubothub/spec/controllers/signup_controller_spec.rb
+++ meeting_4/rubothub/spec/controllers/signup_controller_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe SignupController, type: :controller do
+  describe '#new' do
+    before do
+      get :new
+    end
 
+    it 'should be a success' do
+      expect(response).to be_ok
+    end
+
+    it 'should have a user' do
+      expect(assigns[:user]).to be
+    end
+
+    it 'should render new template' do
+      expect(response).to render_template :new
+    end
+  end
+
+  #describe '#create' do
+  #  before do
+  #    post :create
+  #  end
+
+  #  it 'should be a success' do
+  #    expect(response).not_to be_ok
+  #  end
+  #end
 end
